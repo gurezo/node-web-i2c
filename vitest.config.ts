ed72f4273754794d690134ac40ee745a72663b39
akihiko.kigure <akihiko.kigure@gmail.com>
2025-04-27 21:34:54 +0900

    update vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,9 +3,5 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     environment: 'jsdom',
-    coverage: {
-      provider: 'v8',
-      reporter: ['text', 'json', 'html'],
-    },
   },
 });
